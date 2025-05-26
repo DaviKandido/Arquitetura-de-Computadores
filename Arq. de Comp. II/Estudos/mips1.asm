cece5358e32a17393dc0d2ca179747ceea6aa727
= <davicandidopucminas@gmail.com>
2025-05-26 17:11:57 -0300

    comit

---
+++ Arq. de Comp. II/Estudos/mips1.asm
@@ -0,0 +1,10 @@
+
+
+.text
+
+.globl main
+
+main:
+addi $s0,$zero,5
+addi $s1,$zero,2
+div $s0,$s1
---
+++ Arq. de Comp. II/Estudos/teste.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Program{
+    class Program{
+        static void main(string[] args){
+            int x = 5;
+            int y = 2;
+            int z = x + y;
+            Console.WriteLine(z);   
+        }
+    }
+
+}
